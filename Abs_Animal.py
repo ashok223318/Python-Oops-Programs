d8c661e7dfd75b80b978bbb48f9afc3a378dcea4
Ashok Andra <andraashok223318@gmail.com>
2026-08-01 18:00:00 +0530

    Added Abs_Animal in python

---
+++ Abs_Animal.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    
+    @abstractmethod
+    
+    def sound(self):
+        pass
+    
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+        
+class Cat(Animal):
+    def sound(self):
+        print("Mewo")
+        
+d = Dog()
+d.sound()
+c = Cat()
+c.sound()                
+            
